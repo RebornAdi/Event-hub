0469727a8c1228afdc49a1833d55dca7386e75f0
RebornAdi <145378359+RebornAdi@users.noreply.github.com>
2024-07-27 18:10:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const exploreButton = document.getElementById('explore-btn');
-    
-    exploreButton.addEventListener('click', () => {
-        alert('Button clicked! More features can be added here.');
-    });
-});
