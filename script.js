2d2f6f86919ea6053fd81aa5aada62d6d163404b
NityaThaker <nityathaker252004@gmail.com>
2024-07-27 11:21:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const exploreButton = document.getElementById('explore-btn');
+    
+    exploreButton.addEventListener('click', () => {
+        alert('Button clicked! More features can be added here.');
+    });
+});
